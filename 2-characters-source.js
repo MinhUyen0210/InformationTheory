21b7c23841ce509370ba071f8cfcbc5ac2fe3772
MinhUyen0210 <56840741+MinhUyen0210@users.noreply.github.com>
2020-11-25 13:39:48 +0700

    edit 2 source

--- 2-characters-source.js
+++ 2-characters-source.js
@@ -7,36 +7,96 @@ function myFunction(){
     number = parseInt(document.getElementById("number").value);
     pa0 = parseFloat(document.getElementById("p(a0)").value);
     //document.getElementById("pa0").innerHTML = "p(a0) = "+ pa0;
-
     pa1 = 1-pa0;
+
+    let pa = [pa0, pa1];
+
+    
     pb0a0 = parseFloat(document.getElementById("p(b0|a0)").value);
     pb0a1 = parseFloat(document.getElementById("p(b0|a1)").value);
     pb1a0 = parseFloat(document.getElementById("p(b1|a0)").value);
     pb1a1 = parseFloat(document.getElementById("p(b1|a1)").value);
     
-    HA = pa0*log2(1/pa0)+pa1*log2(1/pa1);
-    document.getElementById("HA").innerHTML = "H(A) = "+ HA.toFixed(number);
+    let matrix = [ 
+        [ pb0a0, pb1a0 ],
+        [ pb0a1, pb1a1 ]
+    ]
 
-    pb0 = pa0*pb0a0 + pa1*pb0a1;
-    document.getElementById("pb0").innerHTML = "p(b0) = "+ pb0.toFixed(number);
-    pb1 = 1 - pb0;
-    document.getElementById("pb1").innerHTML = "p(b1) = "+ pb1.toFixed(number);
+    let pb = []
+    
+    let length = pa.length;
+    
+    for (let i = 0; i < length; i++) {
+    let temp = 0;
+    for (let j = 0; j < length; j++) {
+    temp+= matrix[j][i]*pa[j];
+    }
+    pb.push(temp);
+    }
+    
+    let pAwithConditionB = [];
+    let probabilitySync = [];
+    let pBwithConditionA = [];
+    
+    for (let i = 0; i < length; i++) {
+    probabilitySync[i] = [];
+    pAwithConditionB[i] = [];
+    for (let j = 0; j < length; j++) {
+    probabilitySync[i].push(matrix[i][j] * pa[i]);
+    pAwithConditionB[i].push(matrix[i][j] * pa[i] / pb[j]);
+    }
+    }
+    
+    let entropyAwithConditionnB = 0;
+    for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+    entropyAwithConditionnB += probabilitySync[i][j] * Math.log2(1 / pAwithConditionB[i][j]);
+    }
+    }
+    
+    let entropyBwithConditionnA = 0;
+    for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+    entropyBwithConditionnA += probabilitySync[i][j] * Math.log2(1 / matrix[i][j]);
+    }
+    }
+    
+    let entropyA = 0;
+    for (let i = 0; i < length; i++) {
+    entropyA += pa[i] * Math.log2(1 / pa[i]);
+    }
+    
+    let entropyB = 0;
+    for (let i = 0; i < length; i++) {
+    entropyB += pb[i] * Math.log2(1 / pb[i]);
+    }
+    
+    let result = entropyA - entropyAwithConditionnB;
+    let entropySync = entropyA + entropyAwithConditionnB;
+    
+    console.log("H(A) = " + round(entropyA));
+    console.log("H(B) = " + round(entropyB));
+    console.log("H(A/B) = ", round(entropyAwithConditionnB));
+    console.log("H(B/A) = ", round(entropyBwithConditionnA));
+    console.log("H(A,B) = ", round(entropySync));
+    console.log("I(A;B) = " + round(result));
+    
+    document.getElementById("HA").innerHTML = "H(A) = "+ round(entropyA);
 
-    HB = pb0*log2(1/pb0)+pb1*log2(1/pb1);
-    document.getElementById("HB").innerHTML = "H(B) = "+ HB.toFixed(number);
+    document.getElementById("HB").innerHTML = "H(B) = "+ round(entropyB);
+    
+    document.getElementById("HBA").innerHTML = "H(B|A) = "+ round(entropyBwithConditionnA);
+    
+    document.getElementById("HAB").innerHTML = "H(A|B) = "+ round(entropyAwithConditionnB);
+    
+    document.getElementById("HAandB").innerHTML = "H(A,B) = "+ round(entropySync);
+    
+    document.getElementById("IAB").innerHTML = "I(A;B) = "+ round(result) + "bits/symbol";
+    
+    // document.getElementById("pb0").innerHTML = "p(b0) = "+ pb0.toFixed(number);
 
-    pb0_a0=pa0*pb0a0;
-    pb0_a1=pa1*pb0a1;
-    pb1_a0=pa0*pb1a0;
-    pb1_a1=pa1*pb1a1;
-
-    HBA = pb0_a0*log2(1/pb0a0)+pb0_a1*log2(1/pb0a1)+pb1_a0*log2(1/pb1a0)+pb1_a1*log2(1/pb1a1)
-    document.getElementById("HBA").innerHTML = "H(B|A) = "+ HBA.toFixed(number);
-
-    IAB = HB-HBA;
-    document.getElementById("IAB").innerHTML = "I(A;B) = "+ IAB.toFixed(number) + "bits/symbol";
-
+    // document.getElementById("pb1").innerHTML = "p(b1) = "+ pb1.toFixed(number);
 }
-//console.log("[p(b0|a0)", " ", "p(b1|a0) \n", "p(b0|a1)"," ", "p(b1|a1)] =");
-//console.log(0.16*log2(1/0.4)+ 0.1*log2(1/0.5)+0.16*log2(1/0.4)+0.08*log2(1/0.2)+0.05*log2(1/0.25)+0.1*log2(1/0.25)+0.16*log2(1/0.4)+0.05*log2(1/0.25)+0.14*log2(1/0.35));
-//console.log(2/3*log2(3/2)+1/3*log2(3));
+function round (t) {
+    return Math.round((t + Number.EPSILON) * 1000) / 1000
+}
